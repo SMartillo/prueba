c92940347993696b13f3efa6637df167d8adea70
sergio.martinezf <sergio.martinezf@AULA13-PC21.JUANXXIII.net>
2024-02-16 20:06:19 +0100

    This is my first commit

---
+++ HelloWorld/src/hola/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package hola;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
